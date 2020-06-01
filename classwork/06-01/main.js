5d558d94b721d6238f3168950affeaf64f131a2d
Shahid <shahid.sarker@gmail.com>
2020-06-01 13:34:40 -0400

    class demo files

---
+++ classwork/06-01/main.js
@@ -0,0 +1,14 @@
+console.log("loaded");
+const paraOne = document.getElementById("paraOne");
+
+paraOne.addEventListener("click", (event) => alert("Clicked first paragraph!"));
+
+// const loginSubmit = document.getElementById("loginSubmit");
+const loginSubmit = document.getElementById("loginSubmit");
+
+const loginDiv = document.getElementById("login");
+
+// loginSubmit.addEventListener("click", (e) => {
+//   //   e.preventDefault();
+//   //   loginDiv.innerHTML = "<h1>Thanks for entering your info</h1>";
+// });
